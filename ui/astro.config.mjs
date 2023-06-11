08340a5aebef5dd0b983e41562915b25a07abd8c
sRRRs <srrrs4510@gmail.com>
2023-06-11 16:21:10 +0900

    delete astro.config copy

--- ui/astro.config.mjs
+++ ui/astro.config.mjs
@@ -1,12 +1,12 @@
-import { defineConfig } from "astro/config";
-import svelte from "@astrojs/svelte";
-import node from "@astrojs/node";
+import { defineConfig } from 'astro/config';
+import svelte from '@astrojs/svelte';
+import node from '@astrojs/node';
 
 // https://astro.build/config
 export default defineConfig({
-  output: "server",
+  output: 'server',
   integrations: [svelte()],
   adapter: node({
-    mode: "standalone",
-  }),
+    mode: 'standalone'
+  })
 });
